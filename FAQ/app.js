cab646b04bdaf338f58bd29847db13ca43715195
Francisco Manuel <fsaa93@gmail.com>
2023-05-30 22:52:00 +0200

    FAQ done
    HTML #FAQ exercise done.

---
+++ FAQ/app.js
@@ -0,0 +1,21 @@
+let clickOne = document.getElementById('1');
+let clickTwo = document.getElementById('2');
+let clickThree = document.getElementById('3');
+let clickFour= document.getElementById('4');
+let clickFive = document.getElementById('5');
+
+clickOne.onclick = function move() {
+    window.location.href='#fEnd';
+}
+clickTwo.onclick = function move() {
+    window.location.href='#html';
+}
+clickThree.onclick = function move() {
+    window.location.href='#css';
+}
+clickFour.onclick = function move() {
+    window.location.href='#js';
+}
+clickFive.onclick = function move() {
+    window.location.href='#bEnd';
+}
